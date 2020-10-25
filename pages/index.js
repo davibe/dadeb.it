f2031c2fb1a72e6a0e93571a5564cc40b0c896dd
Davide Bertola <dade@dadeb.it>
2020-10-25 16:38:17 +0100

    better image background

--- pages/index.js
+++ pages/index.js
@@ -46,8 +46,24 @@ const Title = () =>
 
 const Index = () => (
   <div>
+    <Head>
+      <style dangerouslySetInnerHTML={{ 
+        __html: `
+          body, html {
+            margin: 0;
+            padding: 0;
+            width: 100%; 
+            height: 100%; 
+            overflow: hidden;
+            background-image: url("static/drawings/home.jpeg");
+            background-repeat: no-repeat;
+            background-size: 210%;
+            background-attachment: fixed;
+          }
+        `
+      }} />
+    </Head>
     <Title />
-    <img src="static/drawings/home.jpeg" />
   </div>
 )
 
